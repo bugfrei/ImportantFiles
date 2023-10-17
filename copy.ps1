f845f5d4ceee4611415e322d739a67436c32e8fd
Carsten Schlegel <71234951+bugfrei@users.noreply.github.com>
2023-10-17 04:19:05 +0200

    Update

--- copy.ps1
+++ copy.ps1
@@ -1,15 +1,23 @@
 # .vimrc : Für 'vim', 'mvim'
-copy ~/.vimrc ./home
+# -> ~
+copy ~/.vimrc ./home -Force
 
 # .vimrccode : Für Visual Studio Code
-copy ~/.vimrccode ./home
+# -> ~
+copy ~/.vimrccode ./home -Force
 
 # /Users/carstenschlegel/.config/nvim
 # Komplette NeoVIM Konfiguration
+# -> /Users/carstenschlegel/.config/nvim
 copy /Users/carstenschlegel/.config/nvim/* ./nvim -Recurse -Force -Container
 
-copy /Users/carstenschlegel/.config/powershell/Microsoft.PowerShell_profile.ps1 .
+# PowerShell Profile
+# -> /Users/carstenschlegel/.config/powershell/Microsoft.PowerShell_profile.ps1
+# -> $profile
+copy /Users/carstenschlegel/.config/powershell/Microsoft.PowerShell_profile.ps1 . -Force
 
+# SSH Keys
+copy /Users/carstenschlegel/.ssh/* ./NoGit/SSH/ -Recurse -Force
 
 
 
